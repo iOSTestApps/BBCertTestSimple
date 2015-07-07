da23f7ccccefdd627ffacddd38c46cf69861dc1f
Christopher Stott <christopherstott@gmail.com>
2015-07-07 09:24:48 -0700

    Initial Commit

---
+++ BBCertTestSimple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BBCertTestSimple
+//
+//  Created by Chris on 2015-07-07.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BBCertTestSimple/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BBCertTestSimple
+//
+//  Created by Chris on 2015-07-07.
+//  Copyright (c) 2015 BB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
